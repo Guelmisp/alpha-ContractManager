e1a07645058a98848a1c14b6becae22a3bc30116
Miguel Pessoa <guelmisp@gmail.com>
2016-06-14 17:39:00 -0300

    Listando todos os usuarios e numeros de dumentos que ele possui

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+	def index
+		@users = User.all
+	end
+
 	def new
 		@user = User.new
 
@@ -33,7 +37,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-		
+
 	end
 
 
