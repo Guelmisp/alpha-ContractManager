efcdea98f169f228182497dc71fceef3c980136b
Miguel Pessoa <guelmisp@gmail.com>
2016-06-17 16:02:00 -0300

    Funcionalidade de Will Paginate Documents adicionada

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
 	end
 	def contractsHome
 		@document  = Document.new
-		@documents = Document.all
+		@documents = Document.paginate(page: params[:page], per_page: 5)
 	end
 	def contractsAbout
 	end
